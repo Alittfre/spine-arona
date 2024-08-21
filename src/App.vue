7513ce2a6c59e7bb147cb08ebf6dd78e387ea1ea
Alittfre <alittfre@gmail.com>
2024-08-21 15:08:05 +0800

    优化动画效果

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <template>
-  <arona_spinePlayer
-    skelUrl="/spine_assets/arona_spr.skel"
-    atlasUrl="/spine_assets/arona_spr.atlas"
-    animation="Idle_01"
-    :backgroundColor="'#00000000'"
-    :alpha="true"
-    :premultipliedAlpha="true"
-    :showControls="true"
-    :zIndex="999"
-  ></arona_spinePlayer>
+  <arona_spinePlayer></arona_spinePlayer>
 </template>
 <script setup lang="ts">
 // @ts-ignore
 import arona_spinePlayer from './components/arona_spinePlayer.vue'
 </script>
-<style scoped lang=""></style>
+<style scoped lang="css"></style>
